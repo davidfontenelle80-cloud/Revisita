aaf554340f177d5a50cfde60ef74764ebb18035b
davidfontenelle80-cloud <davidfontenelle80@gmail.com>
2026-09-23 05:57:51 -0400

    Version map dependency at 1.3.4

--- js/map.js
+++ js/map.js
@@ -1,4 +1,4 @@
-import{TILE_SIZE,clamp,latLngToWorld,worldToLatLng}from'./map-utils.js?v=1.3.3';
+import{TILE_SIZE,clamp,latLngToWorld,worldToLatLng}from'./map-utils.js?v=1.3.4';
 
 export class SimpleMap{
   constructor(el,o={}){
